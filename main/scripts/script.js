6070fc184027e8675ba5325ec32547c27b7a6a4d
wilson3centaurus <tafadzwawilsonsedze@gmail.com>
2024-04-30 13:02:14 +0200

    update 6 files

--- main/scripts/script.js
+++ main/scripts/script.js
@@ -2,3 +2,31 @@ function toggleNav() {
   var navToggle = document.getElementById("nav-toggle");
   navToggle.checked = !navToggle.checked;
 }
+
+const currentDate = new Date();
+
+// Array of month names
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+// Format date as "DD Month YYYY"
+const day = currentDate.getDate();
+const month = monthNames[currentDate.getMonth()];
+const year = currentDate.getFullYear();
+
+console.log("Formatted date:", `${day} ${month} ${year}`);
+
+// Display current date
+document.querySelector(".currentDate").innerText = `${day} ${month} ${year}`;
