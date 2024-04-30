4a98c4a1e9e39fbfd244e3e2a05869f4cc546153
wilson3centaurus <tafadzwawilsonsedze@gmail.com>
2024-04-30 15:55:36 +0200

    update userLogin.js, userLogin.css and userLogin.html

---
+++ main/scripts/userLogin.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBpAI_F_qNfdqr8SmNXlmUIIzrfLGLBfxk",
+  authDomain: "zimunda-smart-farm-project.firebaseapp.com",
+  databaseURL: "https://zimunda-smart-farm-project-default-rtdb.firebaseio.com",
+  projectId: "zimunda-smart-farm-project",
+  storageBucket: "zimunda-smart-farm-project.appspot.com",
+  messagingSenderId: "1018586661772",
+  appId: "1:1018586661772:web:b82e5142e2068e8aa90ca9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
